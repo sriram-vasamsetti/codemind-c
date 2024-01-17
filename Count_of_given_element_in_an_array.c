b5327ca20542ba54e9eab23fa434b69b51783537
Sri ram Vasamsetti <155539588+sriram-vasamsetti@users.noreply.github.com>
2024-01-17 21:17:05 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int x;     int count=0;     scanf("%d",&x);     for(int i=0;i<n;i++){     if(arr[i]==x){         count=count+1;     } } printf("%d",count); }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int x;
+    int count=0;
+    scanf("%d",&x);
+    for(int i=0;i<n;i++){
+    if(arr[i]==x){
+        count=count+1;
+    }
+}
+printf("%d",count);
+}
